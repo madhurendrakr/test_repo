b7c166f831ce3d365249dadfda5259d0c1ba95d0
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-12-12 23:53:25 +0530

    add function

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter a and b : " << endl;
+    cin >> a >> b;
+    int c = a + b;
+    cout << c << endl;
+    return 0;
+}
